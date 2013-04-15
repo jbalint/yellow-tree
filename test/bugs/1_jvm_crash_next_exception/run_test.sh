9dfa6b513b58f3ff06dc8ce83c6f7a5f5a6b9154
Jess Balint <jbalint@gmail.com>
2013-04-15 16:11:35 -0500

    use jni check by default, preload the jsig library

--- test/bugs/1_jvm_crash_next_exception/run_test.sh
+++ test/bugs/1_jvm_crash_next_exception/run_test.sh
@@ -4,4 +4,5 @@ set -x
 javac -g TestBug1.java
 # run the test
 export LD_LIBRARY_PATH=`pwd`/../../..:$LD_LIBRARY_PATH
-java -agentlib:yt=runfile=testbug1.lua -cp . TestBug1
+export LD_PRELOAD=$JAVA_HOME/jre/lib/amd64/libjsig.so
+java -Xcheck:jni -agentlib:yt=runfile=testbug1.lua -cp . TestBug1
