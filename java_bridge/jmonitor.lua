f5a4b431faba00dcd01db40f9aa7db86c5667dd4
Jess Balint <jbalint@gmail.com>
2013-07-17 11:21:44 -0500

    continue naming convention changes to jmonitor class

--- java_bridge/jmonitor.lua
+++ java_bridge/jmonitor.lua
@@ -4,8 +4,9 @@ jmonitor.__index = jmonitor
 -- ============================================================
 -- create a new jmonitor object including raw pointer creation
 function jmonitor.new(name)
-   local jmonitor_raw = lj_create_raw_monitor(name)
-   local self = jmonitor.create(jmonitor_raw)
+   assert(type(name) == "string")
+   local monitor_raw = lj_create_raw_monitor(name)
+   local self = jmonitor.create(monitor_raw)
    self.name = name
    return self
 end
@@ -13,9 +14,10 @@ end
 -- ============================================================
 -- create a new jmonitor object from a raw pointer
 -- jmonitor.new() should be used where possible
-function jmonitor.create(jmonitor_raw)
+function jmonitor.create(monitor_raw)
+   assert(type(monitor_raw) == "userdata")
    local self = {}
-   self.jmonitor_raw = jmonitor_raw
+   self.monitor_raw = monitor_raw
    setmetatable(self, jmonitor)
    return self
 end
@@ -23,37 +25,37 @@ end
 -- ============================================================
 function jmonitor:__tostring()
    return string.format("jmonitor@%s",
-						lj_pointer_to_string(self.jmonitor_raw))
+						lj_pointer_to_string(self.monitor_raw))
 end
 
 -- ============================================================
 function jmonitor:destroy()
-   lj_destroy_raw_monitor(self.jmonitor_raw)
+   lj_destroy_raw_monitor(self.monitor_raw)
 end
 
 -- ============================================================
 function jmonitor:lock()
-   lj_raw_monitor_enter(self.jmonitor_raw)
+   lj_raw_monitor_enter(self.monitor_raw)
 end
 
 -- ============================================================
 function jmonitor:unlock()
-   lj_raw_monitor_exit(self.jmonitor_raw)
+   lj_raw_monitor_exit(self.monitor_raw)
 end
 
 -- ============================================================
 function jmonitor:wait(time)
-   lj_raw_monitor_wait(self.jmonitor_raw, time or 0)
+   lj_raw_monitor_wait(self.monitor_raw, time or 0)
 end
 
 -- ============================================================
 function jmonitor:notify()
-   lj_raw_monitor_notify(self.jmonitor_raw)
+   lj_raw_monitor_notify(self.monitor_raw)
 end
 
 -- ============================================================
 function jmonitor:broadcast()
-   lj_raw_monitor_notify_all(self.jmonitor_raw)
+   lj_raw_monitor_notify_all(self.monitor_raw)
 end
 
 -- ============================================================
