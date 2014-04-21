41388e79e75ade0d1fa1d0dcb66aac709b1a5724
Jess Balint <jbalint@gmail.com>
2014-04-21 15:38:03 -0500

    grimple mode: implement opening class files in a "special" grimple mode

--- grimple-mode.el
+++ grimple-mode.el
@@ -31,55 +31,75 @@
 		     temporary-file-directory))
   "Directory for temporary files made by `grimple-mode.el'."
   :type 'directory
-  :group 'archive)
+  :group 'grimple)
+
+(defcustom grimple-soot-jar
+  nil
+  "Complete path to Soot 2.5.0 jar for Grimple decompilation."
+  :type '(file :must-match t)
+  :group 'grimple)
+
+(defcustom grimple-soot-classpath
+  nil
+  "Extra classpath for Soot Grimple decompilation."
+  :type '(set (const 'directory))
+  :group 'grimple)
+
+(setq grimple-soot-jar
+	  "/home/jbalint/Downloads/soot-2.5.0.jar")
+
+(setq grimple-soot-classpath
+	  '("/home/jbalint/sw/stardog-2.1.3/expand"))
 
 (put 'grimple-class-mode 'mode-class 'special)
-
-(defun grimple-class-name-of-file-process-filter (proc output)
-  ;; TODO does not handle chunking properly
-  ;; TODO does not handle Java 5 version of javap which doesn't give warning
-  (if (string-match "contains\\s-+\\([[:alnum:]\\.]+\\)\\b" output)
-	  (match-string 1 output)))
 
 ;; Find the name of the class contained in the given file by executing
 ;; `javap'. `javap' will not discriminate if the file is not in the
 ;; correct package as determined by the classpath. In Java 6+ a
 ;; warning will be printed containing the complete class name +
 ;; package.
-(defun grimple-class-name-of-file (file-name)
+(defun grimple-classname-of-file (file-name)
   "Find the name of the class contained in the given file"
-  (let ((unqual-class-name (file-name-base file-name)))
-	(set-process-filter
-	 (start-process "grimple-class-finder" nil "javap" "-cp" (file-name-directory file-name) unqual-class-name)
-	 'grimple-class-name-of-file-process-filter)))
+  (let* ((unqual-classname (file-name-base file-name))
+		 (command (mapconcat 'identity `("javap" "-cp" ,(file-name-directory file-name) ,unqual-classname) " "))
+		 (output (shell-command-to-string command)))
+	(if (string-match "contains\\s-+\\([[:alnum:]\\.]+\\)\\b" output)
+		(match-string 1 output))))
 
-(defun grimple-decompile-class (file-name)
-  (let* ((class-name (replace-regexp-in-string "pattern" "replacement" file-name))
-		  (args '("-jar" grimple-soot-jar "-cp" "CLASSPATH" "-f" "grimple" class-name)))
-	(call-process "java" nil nil nil args)))
+(defun grimple-package-of-class (classname)
+  ""
+  (if (string-match "\\(.*\\)\\.[[:alnum:]]+" classname)
+	  (match-string 1 classname)))
 
-(defun grimple-class-mode ()
-  (grimple-mode)
-  (kill-all-local-variables)
-  (setq major-mode 'grimple-class-mode)
-  (setq mode-name "Grimple")
-  (set (make-local-variable 'font-lock-defaults)
-	   '(grimple-font-lock-keywords nil nil nil nil))
-  (grimple-decompile-class buffer-file-name)
-  (font-lock-mode nil)
-  (turn-on-font-lock))
+(defun grimple-decompile-class (classpath-in classname outdir)
+  ""
+  (let* ((rt-jar (concat (getenv "JAVA_HOME") "/jre/lib/rt.jar"))
+		 (classpath (mapconcat 'identity (append `(,rt-jar ,classpath-in) grimple-soot-classpath) ":"))
+		 (args `("-jar" ,grimple-soot-jar "-cp" ,classpath "-f" "grimple" "-output-dir" ,outdir ,classname)))
+	(apply 'call-process (append '("java" nil "**nil" t) args))))
+
+(defun grimple-replace-buffer-contents-with-decompiled (file-name)
+  (let* ((classname (grimple-classname-of-file file-name))
+		 (package-dir (replace-regexp-in-string "\\." "/" (grimple-package-of-class classname)))
+		 (classpath-for-file (replace-regexp-in-string (concat "/" package-dir "/") "" (file-name-directory file-name)))
+		 (grimple-file (concat grimple-tmpdir "/" classname ".grimple")))
+	(grimple-decompile-class classpath-for-file classname grimple-tmpdir)
+	(erase-buffer)
+	(insert-file-contents grimple-file)))
 
 (defun grimple-mode ()
   (interactive)
   (kill-all-local-variables)
   (setq major-mode 'grimple-mode)
   (setq mode-name "Grimple")
+  (if (string-match "\\.class$" buffer-file-name)
+	  (grimple-replace-buffer-contents-with-decompiled buffer-file-name))
   (set (make-local-variable 'font-lock-defaults)
 	   '(grimple-font-lock-keywords nil nil nil nil))
   (font-lock-mode nil)
   (turn-on-font-lock))
 
 (add-to-list 'auto-mode-alist '("\\.grimp\\(le\\)?\\'" . grimple-mode))
-(add-to-list 'auto-mode-alist '("\\.class\\'" . grimple-class-mode))
+(add-to-list 'auto-mode-alist '("\\.class\\'" . grimple-mode))
 
 (provide 'grimple-mode)
